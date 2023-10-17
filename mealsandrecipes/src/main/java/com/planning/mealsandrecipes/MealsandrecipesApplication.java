cacfdd0fd7e90c64ec1adc43e307387e156ac22c
Ritika Deshpande <ritika_gd@outlook.com>
2023-10-17 09:23:25 -0400

    initial commit

---
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/MealsandrecipesApplication.java
@@ -0,0 +1,13 @@
+package com.planning.mealsandrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsandrecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsandrecipesApplication.class, args);
+	}
+
+}
---
+++ mealsandrecipes/src/test/java/com/planning/mealsandrecipes/MealsandrecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planning.mealsandrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsandrecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
