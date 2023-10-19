b17466bc36ae97e8315065a95366af3fb5814eff
Jyotsna <jpenumak@redhat.com>
2023-10-19 14:12:07 -0400

    Ingredient Controller
    Implement the Ingredient Controller

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/entity/Ingredient.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/entity/Ingredient.java
@@ -1,4 +1,4 @@
-package com.planning.mealsandrecipes.entity.ingredient;
+package com.planning.mealsandrecipes.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
