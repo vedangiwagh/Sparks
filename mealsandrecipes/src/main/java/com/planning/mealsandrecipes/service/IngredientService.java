c1494db347c0af028b85b74b3e0a45ef33aa3e60
Jyotsna <jpenumak@redhat.com>
2023-10-19 19:30:35 -0400

    Update IngredientService.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/IngredientService.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/IngredientService.java
@@ -1,36 +1,20 @@
-package com.planning.mealsandrecipes.service;
+public class IngredientService {
+    @Autowired
+    private IngredientRepository IngredientRepository;
 
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.NoSuchElementException;
-
-// Import the Ingredient class
-import com.planning.mealsandrecipes.model.Ingredient;
-
-public interface IngredientService {
-
-    Ingredient get(Long id);
-
-    List<Ingredient> getAll();
-}
-
-@Service
-@AllArgsConstructor
-public class IngredientServiceImpl implements IngredientService {
-
-    private final IngredientRepository ingredientRepository;
-
-    @Override
-    public Ingredient get(Long id) {
-        return ingredientRepository.findById(id).orElseThrow(
-                () -> new NoSuchElementException("Ingredient with id: '" + id + "' was not found.")
-        );
+    public Ingredient save(Ingredient ingredient) {
+        return ingredientRepository.save(ingredient);
     }
 
-    @Override
+    public Ingredient get(Long id) {
+        return ingredientRepository.findById(id).orElse(null);
+    }
+
     public List<Ingredient> getAll() {
         return ingredientRepository.findAll();
+    }
+
+    public void delete(Long id) {
+        ingredientRepository.deleteById(id);
     }
 }
