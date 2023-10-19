6556c5580fb48824d73269a3adb1caecec21a928
Jyotsna <jpenumak@redhat.com>
2023-10-19 19:31:07 -0400

    Update IngredientService.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/IngredientService.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/IngredientService.java
@@ -1,6 +1,11 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class IngredientService {
     @Autowired
-    private IngredientRepository IngredientRepository;
+    private IngredientRepository ingredientRepository;
 
     public Ingredient save(Ingredient ingredient) {
         return ingredientRepository.save(ingredient);
