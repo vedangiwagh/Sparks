18da611936a54b71cf7e7eeff612aef53478aedf
Jyotsna <jpenumak@redhat.com>
2023-10-19 21:12:24 -0400

    Update ClientController.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/ClientController.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/ClientController.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 @RequestMapping("/clients")
 public class ClientController {
 
+    // Inject the ClientService for handling client-related operations.
     private final ClientService clientService;
 
     @Autowired
@@ -25,26 +26,31 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    // Define an endpoint to retrieve all clients.
     @GetMapping
     public List<Client> getAllClients() {
         return clientService.getAllClients();
     }
 
+    // Define an endpoint to retrieve a client by their ID.
     @GetMapping("/{id}")
     public Optional<Client> getClientById(@PathVariable int id) {
         return clientService.getClientById(id);
     }
 
+    // Define an endpoint to create a new client.
     @PostMapping
     public Client createClient(@RequestBody Client client) {
         return clientService.createClient(client);
     }
 
+    // Define an endpoint to update an existing client.
     @PutMapping("/{id}")
     public Client updateClient(@PathVariable int id, @RequestBody Client updatedClient) {
         return clientService.updateClient(id, updatedClient);
     }
 
+    // Define an endpoint to delete a client by their ID.
     @DeleteMapping("/{id}")
     public void deleteClient(@PathVariable int id) {
         clientService.deleteClient(id);
