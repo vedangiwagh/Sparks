a774aa5b700a87872ae83dc21c06a1ec71aa29f9
Surabhiad <56550466+Surabhiad@users.noreply.github.com>
2023-10-19 18:20:23 -0700

    Update RecipeService.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/RecipeService.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/RecipeService.java
@@ -10,22 +10,27 @@ public class RecipeService {
         this.recipeRepository = recipeRepository;
     }
 
+    // Create a new recipe and save it to the repository.
     public Recipe createRecipe(Recipe recipe) {
         return recipeRepository.save(recipe);
     }
 
+    // Retrieve a recipe by its ID.
     public Recipe getRecipeById(Integer recipeId) {
         return recipeRepository.findById(recipeId).orElse(null);
     }
 
+    // Retrieve a list of all recipes.
     public List<Recipe> getAllRecipes() {
         return recipeRepository.findAll();
     }
 
+    // Update an existing recipe with the provided data.
     public void updateRecipe(Recipe recipe) {
         recipeRepository.save(recipe);
     }
 
+    // Delete a recipe by its ID.
     public void deleteRecipe(Integer recipeId) {
         recipeRepository.deleteById(recipeId);
     }
