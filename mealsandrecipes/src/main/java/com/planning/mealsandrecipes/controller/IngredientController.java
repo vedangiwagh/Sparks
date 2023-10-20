21ed91dfe62713ad529cebcaef43677c2874e54a
Surabhiad <56550466+Surabhiad@users.noreply.github.com>
2023-10-19 18:15:04 -0700

    Updated IngredientController.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/IngredientController.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/IngredientController.java
@@ -15,39 +15,44 @@ import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(path = "api/v1/ingredients")
-// origins = {"http://localhost:8000", "https://"},
+// Define CrossOrigin to allow requests from specific origins.
 @CrossOrigin(
         origins = {"http://localhost:8000"},
         maxAge = 3800,
         allowCredentials = "true"
 )
 @AllArgsConstructor
+// Tag the controller for Swagger documentation.
 @Tag(description = "Set of endpoints for ingredients.", name = "Ingredient Controller")
 public class IngredientController {
 
     private final IngredientService ingredientService;
 
+    // Define an endpoint to get a specific ingredient by its ID.
     @GetMapping(value = "/{id}")
     @Operation(summary = "Returns a specific ingredient based on given parameter.")
     public Ingredient getIngredient(@Parameter(description = "ID of the ingredient.", example = "1") @PathVariable Long id) {
         return ingredientService.get(id);
     }
 
+    // Define an endpoint to get a list of all ingredients in the database.
     @GetMapping()
-    @Operation(summary = "Returns list of all ingredients in the database.")
+    @Operation(summary = "Returns a list of all ingredients in the database.")
     public List<Ingredient> getAllIngredients() {
         return ingredientService.getAll();
     }
 
+    // Define exception handling for NoSuchElementException.
     @ExceptionHandler(value = NoSuchElementException.class)
-    public ResponseEntity<?> handleExceptions(NoSuchElementException e) {
+    public ResponseEntity<?> handleNoSuchElementException(NoSuchElementException e) {
         return ResponseEntity
                 .badRequest()
                 .body(new MessageResponse(e.getMessage()));
     }
 
+    // Define a more general exception handler to handle various exceptions.
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<?> handleExceptions(Exception e) {
+    public ResponseEntity<?> handleGenericException(Exception e) {
         String message;
 
         if (e instanceof HttpMessageNotReadableException) {
