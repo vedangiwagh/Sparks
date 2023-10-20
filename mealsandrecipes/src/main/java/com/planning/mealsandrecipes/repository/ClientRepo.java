dd927e6bdeb6376c91fb2902e5d229e718fa6ef3
Jyotsna <jpenumak@redhat.com>
2023-10-19 21:17:11 -0400

    Update ClientRepo.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/repository/ClientRepo.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/repository/ClientRepo.java
@@ -4,7 +4,7 @@ import com.planning.mealsandrecipes.entity.Client;
 
 import org.springframework.data.repository.CrudRepository;
 
+// ClientRepo is a repository interface for the Client entity.
+// It extends the CrudRepository to provide basic CRUD operations for clients.
 public interface ClientRepo extends CrudRepository<Client, Integer> {
 }
-
-
