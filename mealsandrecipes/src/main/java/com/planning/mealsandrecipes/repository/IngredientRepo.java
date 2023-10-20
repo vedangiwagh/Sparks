6a857f443d445de948542ee344d66f4b52f04124
Jyotsna <jpenumak@redhat.com>
2023-10-19 21:17:33 -0400

    Update IngredientRepo.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/repository/IngredientRepo.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/repository/IngredientRepo.java
@@ -3,8 +3,11 @@ package com.planning.mealsandrecipes.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// IngredientRepository is a repository interface for the Ingredient entity.
+// It extends JpaRepository to provide access to CRUD operations for ingredients.
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
+    // Custom query method to check if an ingredient with a given code exists.
     Boolean existsByCode(Long code);
 }
