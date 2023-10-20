d65d0efb911759f102e51421b19501bf04945431
Jyotsna <jpenumak@redhat.com>
2023-10-19 21:19:23 -0400

    Update IngredientService.java

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/IngredientService.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/service/IngredientService.java
@@ -7,18 +7,22 @@ public class IngredientService {
     @Autowired
     private IngredientRepository ingredientRepository;
 
+    // Save an ingredient to the repository.
     public Ingredient save(Ingredient ingredient) {
         return ingredientRepository.save(ingredient);
     }
 
+    // Retrieve an ingredient by its ID.
     public Ingredient get(Long id) {
         return ingredientRepository.findById(id).orElse(null);
     }
 
+    // Retrieve a list of all ingredients.
     public List<Ingredient> getAll() {
         return ingredientRepository.findAll();
     }
 
+    // Delete an ingredient by its ID.
     public void delete(Long id) {
         ingredientRepository.deleteById(id);
     }
