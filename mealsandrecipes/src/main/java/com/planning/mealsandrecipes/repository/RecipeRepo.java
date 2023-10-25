9bcf64d3653e5168a247021a2f8706b3d107f4ac
Ritika Deshpande <ritika_gd@outlook.com>
2023-10-25 01:21:39 -0400

    added find recipe by name

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/repository/RecipeRepo.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/repository/RecipeRepo.java
@@ -2,12 +2,15 @@ package com.planning.mealsandrecipes.repository;
 
 import com.planning.mealsandrecipes.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 // RecipeRepository is a repository interface for the Recipe entity.
 // It extends JpaRepository to provide access to CRUD operations for recipes.
 public interface RecipeRepo extends JpaRepository<Recipe, Long> {
+
+    List<Recipe> findByRecipeName(@Param("recipename")String recipeName);
 
     // Custom query method to retrieve a list of recipes ordered by the updated timestamp in descending order.
 }
