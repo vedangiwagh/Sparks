75541209536b002e0fd5e8566c4ff44e2047c287
Ritika Deshpande <ritika_gd@outlook.com>
2023-10-26 17:10:53 -0400

    updated swagger documentation

--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/ClientController.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.planning.mealsandrecipes.controller;
 import com.planning.mealsandrecipes.entity.Client;
 import com.planning.mealsandrecipes.service.ClientService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,9 @@ public class ClientController {
     private ClientService clientService;
 
     // Define an endpoint to retrieve all clients.
+    @Operation(summary = "Get a list of clients")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of Clients")
+    @ApiResponse(responseCode = "404", description = "no Clients")
     @GetMapping
     public List<Client> getAllClients() {
         return clientService.getAllClients();
@@ -30,11 +34,16 @@ public class ClientController {
 
     // Define an endpoint to retrieve a client by their ID.
     @GetMapping("/{id}")
+    @Operation(summary = "Get client by id")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of items by id")
+    @ApiResponse(responseCode = "404", description = "Client not exist with id")
     public Client getClientById(@PathVariable int id) {
         return clientService.getClientById(id);
     }
 
     // Define an endpoint to create a new client.
+    @Operation(summary = "Create a new client")
+    @ApiResponse(responseCode = "201", description = "client created successfully")
     @PostMapping
     public Client createClient(@RequestBody Client client) {
         return clientService.createClient(client);
@@ -42,12 +51,16 @@ public class ClientController {
 
     // Define an endpoint to update an existing client.
     @PutMapping("/{id}")
+    @Operation(summary = "Update client")
+    @ApiResponse(responseCode = "201", description = "client  successfully")
     public Client updateClient(@PathVariable int id, @RequestBody Client updatedClient) {
         return clientService.updateClient(id, updatedClient);
     }
 
     // Define an endpoint to delete a client by their ID.
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete client")
+    @ApiResponse(responseCode = "200", description = "client deleted successfully")
     public void deleteClient(@PathVariable int id) {
         clientService.deleteClient(id);
     }
--- mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/MealPlanController.java
+++ mealsandrecipes/src/main/java/com/planning/mealsandrecipes/controller/MealPlanController.java
@@ -1,6 +1,8 @@
 package com.planning.mealsandrecipes.controller;
 import com.planning.mealsandrecipes.MealModel;
 import com.planning.mealsandrecipes.service.MealPlanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +15,17 @@ public class MealPlanController {
     private MealPlanService mealPlanService;
 
 
+    @Operation(summary = "Returns a specific mealPlan based on given parameters.")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of mealPlans")
+    @ApiResponse(responseCode = "404", description = "no mealPlan")
     @GetMapping("/{mealType}/{recipeType}")
     public List<MealModel> getMeal(@PathVariable String mealType, @PathVariable String recipeType) {
         return mealPlanService.getMeal(mealType, recipeType);
     }
 
+    @Operation(summary = "Returns a specific mealPlan based on given parameters specific to client.")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of mealPlan")
+    @ApiResponse(responseCode = "404", description = "no mealPlan for client")
     @GetMapping("/{mealType}/{recipeType}/{clientId}/{clientName}")
     public List<MealModel> getMealClientSpecific(@PathVariable String mealType, @PathVariable String recipeType,
                                    @PathVariable int clientId, @PathVariable String clientName) {
